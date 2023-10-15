6b80b3a30778a1314f12b5985890a8ae79846746
leiregesteira <leiregesteira@gmail.com>
2023-10-15 11:12:12 +0200

    error

--- src/main/java/domain/Sport.java
+++ src/main/java/domain/Sport.java
@@ -12,7 +12,7 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlID;
 
-import org.junit.Test;
+
 
 @XmlAccessorType(XmlAccessType.FIELD)
 @Entity
